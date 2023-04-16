705bce7293fa75f73919599e44c96ba2d3dcfe34
Swaruppaul21 <93387666+Swaruppaul21@users.noreply.github.com>
2023-04-16 20:22:22 +0530

    Add files via upload

---
+++ portfolio_web/app.js
@@ -0,0 +1,8 @@
+history.scrollRestoration = 'manual';
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
